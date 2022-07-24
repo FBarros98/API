65dd606f69dc1eee575adf1bb6dc045539c1bb35
FBarros98 <fernandabarros@hotmail.com.br>
2022-07-23 23:28:44 -0300

    Change data format

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import * 
-import json, time
+import json
+from time import *
 
 #Criar o aplicativo Flask - Para a construção dessa API foi utilizado o Flask, que é um micro framework para Python utilizado para criar aplicativos Web.
 app = Flask(__name__)
@@ -8,7 +9,8 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 
 def home_page():
-    data_set = {'Page': 'Home', 'Message': 'Successfully loaded the Home page', 'Timestamp':time.time()} # Conjunto de dados que é um dicionário com todas as infos que a API deve retornar
+    data = strftime("%a, %d %b %Y %H:%M:%S", localtime())
+    data_set = {'Page': 'Home', 'Message': 'Welcome to the Pagar.me API', 'Timestamp':data} # Conjunto de dados que é um dicionário com todas as infos que a API deve retornar
     json_dump = json.dumps(data_set) #Converte as informações em um json
 
     return json_dump
@@ -17,8 +19,9 @@ def home_page():
 @app.route('/user/', methods=['GET'])
 
 def request_page():
+    data = strftime("%a, %d %b %Y %H:%M:%S", localtime())
     user_query = str(request.args.get('user')) #O endpoint passa a ser /user/?user=asdfaddsaf
-    data_set = {'Page': 'Request', 'Message': f'Successfully got the request for {user_query}', 'Timestamp':time.time()} # Conjunto de dados que é um dicionário com todas as infos que a API deve retornar
+    data_set = {'Page': 'Request', 'Message': f'Successfully got the request for {user_query}', 'Timestamp':data} # Conjunto de dados que é um dicionário com todas as infos que a API deve retornar
     json_dump = json.dumps(data_set) #Converte as informações em um json
 
     return json_dump
