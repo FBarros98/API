512d3d1b87e0547c49824ad4714810ad6f4d8d11
FBarros98 <fernandabarros@hotmail.com.br>
2022-07-26 18:16:27 -0300

    Update time

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import * 
 import json
 from time import *
-
+from pytz import *
+from datetime import datetime
 #Criar o aplicativo Flask - Para a construção dessa API foi utilizado o Flask, que é um micro framework para Python utilizado para criar aplicativos Web.
 app = Flask(__name__)
 
@@ -9,7 +10,10 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 
 def home_page():
-    data = strftime("%a, %d %b %Y %H:%M:%S", localtime())
+    IST = timezone('America/Sao_Paulo') 
+    data_atual = datetime.now(IST)
+    data_local = data_atual.astimezone(IST) 
+    data = data_local.strftime('%a, %d %b %Y %H:%M:%S')
     data_set = {'Page': 'Home', 'Message': 'Welcome to the Pagar.me API', 'Timestamp':data} # Conjunto de dados que é um dicionário com todas as infos que a API deve retornar
     json_dump = json.dumps(data_set) #Converte as informações em um json
 
@@ -19,7 +23,10 @@ def home_page():
 @app.route('/user/', methods=['GET'])
 
 def request_page():
-    data = strftime("%a, %d %b %Y %H:%M:%S", localtime())
+    IST = timezone('America/Sao_Paulo') 
+    data_atual = datetime.now(IST)
+    data_local = data_atual.astimezone(IST) 
+    data = data_local.strftime('%a, %d %b %Y %H:%M:%S')
     user_query = str(request.args.get('user')) #O endpoint passa a ser /user/?user=usuario_de_sua_escolha
     data_set = {'Page': 'Request', 'Message': f'Welcome to the Pagar.me API. Successfully got the request for {user_query}', 'Timestamp':data} # Conjunto de dados que é um dicionário com todas as infos que a API deve retornar
     json_dump = json.dumps(data_set) #Converte as informações em um json
